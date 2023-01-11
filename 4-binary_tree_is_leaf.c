0562891a66afbcd9d541ac0ec66303133c834f43
JoaquinFurta <joaquinfurta2@gmail.com>
2023-01-11 11:58:34 -0800

    task_4_betty_fix

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,12 +2,13 @@
 /**
  * binary_tree_is_leaf - deletes an entire binary tree
  * @node: pointer to the tree to delete
+ * Return: (1) if succed, (0) if it fails
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
-	
+
 	if (!node->left && !node->right)
 		return (1);
 	else

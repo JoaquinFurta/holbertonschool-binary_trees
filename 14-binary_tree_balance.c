1b3e4cded9e16e2dbf3cfb601b39c8b6859c7bcd
LCobham <lucascobham@hotmail.com>
2023-01-12 04:20:14 -0800

    Fixed bugs in task 14 and in file 0-binary_tree_node.c (compilation issues)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -40,9 +40,11 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left && !tree->right)
-		return (1);
+		return (1 + binary_tree_height(tree->left) -
+			binary_tree_height(tree->right));
 	if (tree->right && !tree->left)
-		return (-1);
+		return (binary_tree_height(tree->left) -
+			(binary_tree_height(tree->right) + 1));
 
 	return (binary_tree_height(tree->left) -
 			binary_tree_height(tree->right));

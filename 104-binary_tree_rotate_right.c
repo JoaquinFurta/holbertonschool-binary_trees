5c31d5c0ebd02015641a5cd1fd723901344e8866
LCobham <lucascobham@hotmail.com>
2023-01-14 07:33:44 -0800

    Created 104-binary_tree_rotate_right.c

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - performa a right rotation on a binary tree
+ * @tree: root of the tree before rotation
+ *
+ * Return: pointer to new root
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	if (!tree)
+		return (NULL);
+
+	tree->parent = tree->left;
+	if (tree->parent)
+	{
+		tree->left = tree->parent->right;
+		tree->parent->right = tree;
+		tree->parent->parent = NULL;
+	}
+	return (tree->parent);
+}

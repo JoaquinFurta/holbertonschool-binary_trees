845f26b3bf1c802a71b73f75bc057ea2fedc143e
LCobham <lucascobham@hotmail.com>
2023-01-14 07:25:28 -0800

    First iteration of 103 rotl task

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - perform a left-rotation on a binary tree
+ * @tree: root of the tree
+ *
+ * Return: pointer to the new root
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	if (!tree)
+		return (NULL);
+
+	tree->parent = tree->right;
+	if (tree->parent)
+	{
+		tree->right = tree->parent->left;
+		tree->parent->left = tree;
+		tree->parent->parent = NULL;
+	}
+	return (tree->parent);
+}

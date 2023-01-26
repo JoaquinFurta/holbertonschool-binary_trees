94e76eeb2b87d837c60fcb9299207d717e143117
LCobham <lucascobham@hotmail.com>
2023-01-26 07:10:55 -0800

    Added src code of size and height functions (included in 131 but checker isnt using them on compilation)

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 /**
  * heap_to_sorted_array - implement heapsort from a max heap
@@ -25,4 +26,32 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 		array[i] = heap_extract(&heap);
 
 	return (array);
+}
+
+/**
+ * binary_tree_size - get the size of a binary tree
+ *
+ * @tree: pointer to root of the tree
+ *
+ * Return: size of binary tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * binary_tree_height - calculate the height of a binary tree
+ * @tree: root node
+ *
+ * Return: height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	return (MAX(1 + binary_tree_height(tree->left),
+			1 + binary_tree_height(tree->right)));
 }

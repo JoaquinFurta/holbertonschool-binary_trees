8506d6fe7ca87c9e7339022eb6f1a4b091580121
LCobham <lucascobham@hotmail.com>
2023-01-26 08:07:48 -0800

    Trying task 134 now that 133 is fixed

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -27,31 +27,3 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 
 	return (array);
 }
-
-/**
- * binary_tree_size - get the size of a binary tree
- *
- * @tree: pointer to root of the tree
- *
- * Return: size of binary tree
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
-}
-
-/**
- * binary_tree_height - calculate the height of a binary tree
- * @tree: root node
- *
- * Return: height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (!tree || (!tree->left && !tree->right))
-		return (0);
-	return (MAX(1 + binary_tree_height(tree->left),
-			1 + binary_tree_height(tree->right)));
-}
